e660a9ad041d7cb7d46b3c8267a8ef7e32ff7306
Caleb Hopkins <CalebHopkins@outlook.com>
2021-03-09 17:24:59 -0600

    Deploying to heroku

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+// Connect to mongo database
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/budget_db",
   {
