ce78e9a34a5412dfc826ce87c377f729ea15aaf1
Caleb Hopkins <CalebHopkins@outlook.com>
2021-03-09 15:47:17 -0600

    Added SW Fetch function

--- public/service-worker.js
+++ public/service-worker.js
@@ -44,3 +44,50 @@ self.addEventListener("activate", event => {
             .then(() => self.clients.claim())
     );
 });
+
+// Fetching (Caches successful GET requests to API and grabs from cache if network request fails)
+self.addEventListener("fetch", event => {
+    // Doesnt cache non-GET requests
+    if (
+        event.request.method !== "GET" ||
+        !event.request.url.startsWith(self.location.origin)
+    ) {
+        event.respondWith(fetch(event.request));
+        return;
+    }
+
+    // Handle RUN_TIME_CACHE GET requests from /api routes
+    if (event.request.url.includes("/api/")) {
+    // Make network request. Go to cache if fails (offline).
+        event.respondWith(
+            caches.open(RUN_TIME_CACHE).then(cache => {
+                return fetch(event.request)
+                    .then(response => {
+                        cache.put(event.request, response.clone());
+                        return response;
+                    })
+                    .catch(() => caches.match(event.request));
+            })
+        );
+        return;
+    }
+
+    //cache first for all other requests to increase performance
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+
+            //request is not in cache, then make network request and cache the response
+            return caches.open(RUN_TIME_CACHE).then(cache => {
+                return fetch(event.request).then(response => {
+                    return cache.put(event.request, response.clone()).then(() => {
+                        return response;
+                    })
+                })
+            })
+        })
+    )
+
+})
