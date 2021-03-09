1d2e69841e1fe77252dab9c31e6d077a39d2982a
Caleb Hopkins <CalebHopkins@outlook.com>
2021-03-09 17:02:40 -0600

    Deploying

--- server.js
+++ server.js
@@ -15,11 +15,6 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget_db", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/budget_db",
   {
