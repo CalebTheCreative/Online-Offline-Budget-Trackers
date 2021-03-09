e1ff7649aa537134a9225c5d71a3bd53f559e7cb
Caleb Hopkins <CalebHopkins@outlook.com>
2021-03-09 15:45:42 -0600

    Added SW installation function.

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,3 +8,17 @@ const FILES_TO_CACHE = [
     "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
     "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
 ]
+
+const STATIC_CACHE = "static-cache-v1";
+const RUN_TIME_CACHE = "run-time-cache-v1";
+
+// Installation
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(STATIC_CACHE).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
