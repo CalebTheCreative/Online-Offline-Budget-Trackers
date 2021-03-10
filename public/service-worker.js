047b8097af11c8220cbc82ae3156be4dc720600a
Caleb Hopkins <CalebHopkins@outlook.com>
2021-03-09 20:17:24 -0600

    Updated service worker.

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,6 +3,7 @@ const FILES_TO_CACHE = [
     "/index.html",
     "/assets/css/style.css",
     "/assets/js/index.js",
+    "/assets/js/indexedDB.js",
     "/assets/images/icons/icon-192x192.png",
     "/assets/images/icons/icon-512x512.png",
     "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
@@ -15,7 +16,7 @@ const RUN_TIME_CACHE = "run-time-cache-v1";
 // Installation
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(STATIC_CACHE).then((cache) => {
+    caches.open(STATIC_CACHE).then(cache  => {
       return cache.addAll(FILES_TO_CACHE);
     })
   );
