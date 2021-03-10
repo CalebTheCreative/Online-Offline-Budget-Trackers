ffdb2cd5f5f363f7c2647989f9fe7a65a14884db
Caleb Hopkins <CalebHopkins@outlook.com>
2021-03-09 21:10:36 -0600

    Added Offline>Online function

--- public/assets/js/indexedDB.js
+++ public/assets/js/indexedDB.js
@@ -27,4 +27,31 @@ function saveRecord(record) {
     store.add(record);    
 };
 
+// Checks for any changes made when offline and stores in server
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
 
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                const transaction = db.transaction(["pending"], "readwrite");
+                const store = transaction.objectStore("pending");
+                store.clear();
+            });
+        }
+    };
+}
+
+// Checks for when the app is back online again
+window.addEventListener("online", checkDatabase);
