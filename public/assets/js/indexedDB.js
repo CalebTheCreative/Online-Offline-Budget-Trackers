607ab062de4bade315bd904186aad0b03736eef4
Caleb Hopkins <CalebHopkins@outlook.com>
2021-03-09 20:16:26 -0600

    Added cache file.

--- public/assets/js/indexedDB.js
+++ public/assets/js/indexedDB.js
@@ -3,7 +3,19 @@ const request = indexedDB.open("budget_db", 1);
 
 // Creates object store("pending")
 request.onupgradeneeded = function(evt){
-    const db = evt.request.result;
+    const db = evt.target.result;
     db.createObjectStore("pending", { autoIncrement: true });
 };
 
+// Checks if onlune
+request.onsuccess = function(evt) {
+    db = evt.target.result;
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+// Reports any errors in the console.log
+request.onerror = function (evt) {
+    console.log("ISSUE! " + evt.target.errorCode);
+};
