b595a4e1abcaeed01da1f67365fead94423b51ac
Caleb Hopkins <CalebHopkins@outlook.com>
2021-03-09 23:52:08 -0600

    Fixed caching issue.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,12 @@
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    "/assets/css/style.css",
+    "/assets/css/styles.css",
     "/assets/js/index.js",
     "/assets/js/indexedDB.js",
     "/assets/images/icons/icon-192x192.png",
     "/assets/images/icons/icon-512x512.png",
+    "/manifest.webmanifest",
     "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
     "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
 ]
